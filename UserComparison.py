d3fea45f7086ab29495df70880f437751477e0ba
Echocage <preston3050@gmail.com>
2014-04-05 15:59:48 -0700

    Added user comparison grapher

---
+++ UserComparison.py
@@ -0,0 +1,35 @@
+import sqlite3
+import numpy as np
+from pylab import *
+
+colors = ['g', 'y', 'r', 'b']
+
+
+def getColor():
+    return colors.pop()
+
+
+con = sqlite3.connect('C:/data/FacebookFriendsData.db')
+c = con.cursor()
+
+user = ["Kevin Gladnick", "Tanner Mindak", 'Bo Oelkers']
+times = [[0] * 24 for x in user]
+for row in c.execute(
+                'SELECT * FROM CodeTable WHERE name IN ' + ([x for x in user].__str__()).replace('[', '(').replace(']',
+                                                                                                                   ')')):
+    nTime = datetime.datetime.fromtimestamp(row[0]).hour
+    times[user.index(row[1])][int(nTime)] += 1
+
+index = np.arange(24)
+bars = []
+width = .25
+for x in times:
+    bars.append(plt.bar(index + times.index(x) * width, x, color=getColor(), label=user[times.index(x)], width=width))
+
+plt.xlabel("Hours since midnight")
+plt.ylabel("Number of times online recorded")
+plt.xlim((0, 24))
+plt.title([x for x in user].__str__() + "'s Facebook useage")
+plt.tight_layout()
+plt.legend()
+plt.show()
