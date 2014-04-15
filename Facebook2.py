3806786baa492ca6990f20432efa4b6e762acd08
Echocage <Preston3050@gmail.com>
2014-04-14 22:37:12 -0700

    Got new version up and running, everything is looking great so far, will be replacing main data collector after a bit more stability testing.

--- Facebook2.py
+++ Facebook2.py
@@ -1,6 +1,8 @@
 import json
 import sqlite3
 import urllib
+import time
+import threading
 from Token import TokenKey
 
 
@@ -19,10 +21,43 @@ def LogUsers():
     params = urllib.urlencode({'q': query, 'access_token': TokenKey})
     url = "https://graph.facebook.com/fql?" + params
     data = urllib.urlopen(url).read()
+    currentTime = time.time()
+    timestampId = [x for x in c.execute('SELECT * FROM timestampids WHERE timestamp = ' + str(currentTime) + '')]
+    if len(timestampId) == 0:
+        try:
+            timestampId = [x for x in c.execute("SELECT * from timestampids ORDER BY id DESC LIMIT 1")][0][1] + 1
+        except:
+            print 'New Timestamp'
+            timestampId = 1
+    else:
+        timestampId = timestampId[0][1]
+    c.execute('INSERT INTO Timestampids VALUES (?,?)', [currentTime, timestampId])
     for i in json.loads(data)['data']:
         if i['online_presence'] == 'active':
-            None
-    print con.commit()
+            userId = [x for x in c.execute('SELECT * FROM UserIds WHERE user = "' + i['name'] + '"')]
+            if len(userId) == 0:
+                try:
+                    userId = [x for x in c.execute("SELECT * from userIds ORDER BY id DESC LIMIT 1")][0][1] + 1
+                except:
+                    userId = 1
+                c.execute('INSERT INTO UserIds VALUES (?,?)', [i['name'], userId])
+            else:
+                userId = userId[0][1]
+            c.execute('INSERT INTO DataTable VALUES (?,?)', [userId, timestampId])
+    con.commit()
 
 
 LogUsers()
+
+
+def do_every(interval, worker_func, iterations=0):
+    if iterations != 1:
+        threading.Timer(
+            interval,
+            do_every, [interval, worker_func, 0 if iterations == 0 else iterations - 1]
+        ).start()
+
+    worker_func()
+
+
+do_every(15, LogUsers)
